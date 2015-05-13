9ac9496ed325e8eaa8fe0710af289c341a9c21a2
Pattrick Hueper <phueper@hueper.net>
2015-05-13 21:59:00 +0200

    after FIRST_INSTALL
    Signed-off-by: Pattrick Hueper <phueper@hueper.net>

---
+++ typo3/web/typo3conf/LocalConfiguration.php
@@ -0,0 +1,60 @@
+<?php
+return array(
+	'BE' => array(
+		'debug' => FALSE,
+		'explicitADmode' => 'explicitAllow',
+		'installToolPassword' => '$P$CFkhvjb0Z6HStAS1CAAys/QDRscLXN0',
+		'loginSecurityLevel' => 'rsa',
+	),
+	'DB' => array(
+		'database' => 'typo3',
+		'extTablesDefinitionScript' => 'extTables.php',
+		'host' => 'localhost',
+		'password' => 'typo3',
+		'socket' => '',
+		'username' => 'typo3',
+	),
+	'EXT' => array(
+		'extConf' => array(
+			'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
+			'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
+		),
+	),
+	'FE' => array(
+		'activateContentAdapter' => FALSE,
+		'debug' => FALSE,
+		'loginSecurityLevel' => 'rsa',
+	),
+	'GFX' => array(
+		'jpg_quality' => '80',
+	),
+	'SYS' => array(
+		'caching' => array(
+			'cacheConfigurations' => array(
+				'extbase_object' => array(
+					'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+					'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+					'groups' => array(
+						'system',
+					),
+					'options' => array(
+						'defaultLifetime' => 0,
+					),
+				),
+			),
+		),
+		'clearCacheSystem' => FALSE,
+		'compat_version' => '6.2',
+		'devIPmask' => '',
+		'displayErrors' => 0,
+		'enableDeprecationLog' => FALSE,
+		'encryptionKey' => 'c032f3747bd1a4f251b4b226e05ac6be1addb91d11e3863902530939aba5a33cf2279ce70c3daf0d1215c940d6ace5b5',
+		'isInitialInstallationInProgress' => FALSE,
+		'sitename' => 'New TYPO3 site',
+		'sqlDebug' => 0,
+		'systemLogLevel' => 2,
+		't3lib_cs_convMethod' => 'mbstring',
+		't3lib_cs_utils' => 'mbstring',
+	),
+);
+?>
